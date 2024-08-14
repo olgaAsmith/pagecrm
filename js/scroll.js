3de5624f0952a5dc5a13ee67b103af1e2b50d2e7
olga_k_qschool <ms.olgasmith.jj@mail.ru>
2024-08-14 17:11:32 +0300

    fix

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const scrollToTopButton = document.getElementById('scrollToTop');
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 500) {
+      scrollToTopButton.style.display = 'block';
+    } else {
+      scrollToTopButton.style.display = 'none';
+    }
+  });
+
+  scrollToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+});
